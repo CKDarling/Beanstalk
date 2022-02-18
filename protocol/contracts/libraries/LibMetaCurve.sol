9b4cd9a1ae446bd363421b4625fe71d7d059d323
Leonardo Fibonacci <hungdtr@gmail.com>
2022-02-18 09:38:28 -0800

    Update LibMetaCurve.sol

--- protocol/contracts/libraries/LibMetaCurve.sol
+++ protocol/contracts/libraries/LibMetaCurve.sol
@@ -13,15 +13,18 @@ interface IMeta3Curve {
     function totalSupply() external view returns (uint256);
 }
 
+
+
 interface IDepositZapCurve {
     function add_liquidity(uint256[] calldata amounts, uint256 min_mint_amount) external returns (uint256);
-    function remove_liquidity(uint256 _amount, uint256[] calldata min_amounts) external returns (uint256[] calldata);
-    function remove_liquidity_imbalance(uint256[] calldata amounts, uint256 max_burn_amount) external returns (uint256);
     function remove_liquidity_one_coin(uint256 _token_amount, int128 i, uint256 min_amount) external returns (uint256);
     function balances(int128 i) external view returns (uint256);
-    function A() external view returns (uint256);
     function fee() external view returns (uint256);
-    function owner() external view returns (address);
+    function coins(uint256 i) external view returns (address);
+    function get_virtual_price() external view returns (uint256);
+    function calc_token_amount(uint256[] calldata amounts, bool deposit) external view returns (uint256);
+    function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256);
+    function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external;
 }
 
 library LibMetaCurve {
